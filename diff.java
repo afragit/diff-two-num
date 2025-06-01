2490d67a562adbd24123bf5cb806d466fa3967f6
afragit <afra.mylearning@outlook.com>
2025-06-01 11:28:58 +0400

    Create diff.java

---
+++ diff.java
@@ -0,0 +1,11 @@
+public class diff {
+  public static void main(String args[]) {
+    Scanner sc = System.in;
+    System.out.println("Enter first num: ");
+    int a = sc.nextInt();
+    System.out.println("Enter second num: ");
+    int b = sc.nextInt();
+    int diff = b + a;
+    System.out.println("Difference: " + diff);
+  }
+}
